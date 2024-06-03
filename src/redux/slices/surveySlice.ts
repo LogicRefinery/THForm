81696d0590edb85daa463b319ddf41b95a7a57ea
kimtaehyung <xoguddkenl@gmail.com>
2024-06-04 00:19:44 +0900

    MSW, 리덕스 추가 설정

--- src/redux/slices/surveySlice.ts
+++ src/redux/slices/surveySlice.ts
@@ -1,26 +1,32 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+// import { SurveyState } from "../../model/survey";
 import type { RootState } from "../store";
 
-interface SurveyState {
-  value1: number;
-  value2: number;
-  value3: number;
-}
-
-const initialState: SurveyState = {
-  value1: 1,
-  value2: 2,
-  value3: 3,
-};
+const initialState: any = [
+  {
+    id: 1,
+    title: "폼 전체 제목입니다.",
+    description: "본문내용 입니다.",
+    questions: [
+      {
+        id: 1,
+        isRequired: true,
+        questionText: "What is your name?",
+        answerType: 1,
+        answer: ["답변이 들어있는 배열"],
+      },
+    ],
+  },
+];
 
 export const surveySlice = createSlice({
   name: "survey",
   initialState,
   reducers: {
     add: (state, action: PayloadAction<number>) => {
-      state.value1 += action.payload;
-      state.value2 += action.payload;
-      state.value3 += action.payload;
+      // state.value1 += action.payload;
+      // state.value2 += action.payload;
+      // state.value3 += action.payload;
     },
   },
 });
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import surveyReducer from "./slices/surveySlice";
 
-const store = configureStore({
+export const store = configureStore({
   reducer: { survey: surveyReducer },
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
